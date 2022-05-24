37890e3e5ec3b9d1583d7b274047d4111839831e
Sara Marinha <up201906805@fe.up.pt>
2022-05-25 00:02:07 +0100

    page done

---
+++ appfeup/lib/model/entities/moodle/activities/sigarraCourseInfo.dart
@@ -0,0 +1,12 @@
+import 'package:uni/model/entities/moodle/activity.dart';
+
+class SigarraCourseInfo extends Activity
+{
+  String title;
+  String content;
+
+  SigarraCourseInfo()
+  {
+    
+  }
+}

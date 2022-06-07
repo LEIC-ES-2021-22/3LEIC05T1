e16c76edab85b907d1c1d187f29a6ce0df6f5ce4
BrunoGomes <bruno.camgomes@gmail.com>
2022-06-07 16:43:43 +0100

    Add comments

--- appfeup/lib/model/entities/moodle/activities/moodle_page.dart
+++ appfeup/lib/model/entities/moodle/activities/moodle_page.dart
@@ -2,6 +2,8 @@ import 'package:uni/model/entities/moodle/moodle_activity.dart';
 import '../../../utils/moodle_activity_type.dart';
 
 class PageActivity extends MoodleActivity {
+
+  //List has one moodle_page_section
   List<dynamic> content;
 
   PageActivity(int id, String title, String description, this.content)
